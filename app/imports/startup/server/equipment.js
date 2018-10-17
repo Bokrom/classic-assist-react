9046fe6872343254fd0041189ad92d1f5e4c934e
Zachary Freeman <zaksfreeman@gmail.com>
2018-10-17 09:47:58 -0400

    Removed insecure

--- app/imports/startup/server/equipment.js
+++ app/imports/startup/server/equipment.js
@@ -1,21 +1,7 @@
 import { Meteor } from 'meteor/meteor';
 import { Equipments } from '../../api/equipment/equipment.js';
 
-/** Initialize the database with a default data document. */
-function addData(data) {
-  console.log(`  Adding: ${data.name}`);
-  Equipments.insert(data);
-}
-
-/** Initialize the collection if empty. */
-if (Equipments.find().count() === 0) {
-  if (Meteor.settings.defaultEquipmentData) {
-    console.log('Creating default data.');
-    Meteor.settings.defaultEquipmentData.map(data => addData(data));
-  }
-}
-
-/** This subscription publishes only the documents associated with the logged in user */
+/**  */
 Meteor.publish('Equipment', function publish() {
     return Equipments.find();
 });
